da2d46390a1c13ea7aa8ab833f19a43f747da6b6
Qunfei Wu <qunfei.wu@allianz.com>
2023-05-11 15:50:43 +0200

    adding version

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     description="A private hub for huggingface transformers hosting on your AWS/Azure/GCP Cloud",
     entry_points={
